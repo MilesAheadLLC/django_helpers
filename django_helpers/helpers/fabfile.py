5f20e7611d3ea56d7697d23f2157c03c8b679f2f
Moses E. Miles III <vamem9z@gmail.com>
2013-04-04 23:47:49 +0000

    [Delivers #47440533] Add helper functions updateall systask

--- django_helpers/helpers/fabfile.py
+++ django_helpers/helpers/fabfile.py
@@ -1,14 +1,13 @@
+import os
 import importlib
-import sys
-from fabric.api import task
+from fabric.api import task, local, lcd
 
-
-'''
+"""
     Provides helpers for projects using fabric
 
     To use these functions import them in your fabfile from django_helpers.helpers.fabfile import <function name>.
     Importing these functions this way ensures that they will list themselves without a namespace.
-'''
+"""
 
 
 @task()
@@ -18,18 +17,25 @@ def taskhelp(full_function_name, base_module_name='fabfile'):
 
      usage: fab taskhelp:<name from fab --list>
     """
-
+    """
+     Split the fully qualified function name into list[modulename, function_name] this is necessary because of
+     how fabric works. We need to add fabfile to the module name to look up the function later
+    """
     name_split = full_function_name.rsplit('.', 1)
 
+
     if len(name_split) == 1:
+        #if only 1 item it must be the function name get reference to the imported base module and set function name
         module_name = base_module_name
         module = importlib.import_module(module_name)
         function_name = name_split[0]
     else:
+        #if more than 1 item the add the first item in the the name split to the base_module_name and then import it
         module_name = base_module_name + "." + name_split[0]
         module = importlib.import_module(module_name)
         function_name = name_split[1]
 
+    # get a reference to the function be looking up the name of the function in the module
     function = getattr(module, function_name)
 
     print function.__doc__
@@ -37,3 +43,28 @@ def taskhelp(full_function_name, base_module_name='fabfile'):
     return function.__doc__
 
 
+def dev_test_only(fn):
+    """
+     Helper function to test if function is being called in the dev or test environment
+    """
+
+    settings_mod = os.environ.get('DJANGO_SETTINGS_MODULE')
+
+    print settings_mod
+
+    if 'settings.dev'in settings_mod or 'settings.test' in settings_mod:
+        fn()
+        return fn
+    else:
+        msg = 'This task only runs on the development and test environments. Believe me you don\'t want to run this in production.'
+        print msg
+        return msg
+
+
+def update_master(path):
+    """
+     Helper function updates master branch of project it takes in the path of the project
+    """
+    with lcd(path):
+        local('git checkout master')
+        local('git pull origin master')
--- django_helpers/tests/test_fabfile_helpers.py
+++ django_helpers/tests/test_fabfile_helpers.py
@@ -1,11 +1,52 @@
+import os
+import types
+
 import pytest
 
-from django_helpers.helpers.fabfile import taskhelp
+from django_helpers.test_fixtures import fake_fn
+
+from django_helpers.helpers.fabfile import taskhelp, dev_test_only
 
 def test_get_docstring_for_function_in_fabfile():
+    """
+     Test that taskhelp can produce doc string for a function in a fabfile
+    """
     out = taskhelp('level_one_fab_test', base_module_name='django_helpers.test_fixtures.fabfile')
     assert out == 'Level One Test'
 
 def test_get_docstring_for_function_in_fabfile_submodule():
+    """
+     Test that taskhelp can produce a doc string for a function in fabfile submodule
+    """
     out = taskhelp('level_two_fab_test', base_module_name='django_helpers.test_fixtures.fabfile.deploy')
     assert out == 'Level Two Test'
+
+def test_dev_test_only_runs_if_env_is_dev_or_test():
+    """
+     Test that dev test only will run for dev and test environments
+    """
+    original_settings = os.environ['DJANGO_SETTINGS_MODULE']
+
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'rapidresearchregistry.settings.dev'
+    result = dev_test_only(fake_fn)
+    assert isinstance(result, types.FunctionType)
+
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'rapidresearchregistry.settings.test'
+    result = dev_test_only(fake_fn)
+    assert isinstance(result, types.FunctionType)
+
+    #reset setting module to original
+    os.environ['DJANGO_SETTINGS_MODULE'] = original_settings
+
+def test_dev_test_only_does_not_run_it_env_is_not_dev_or_test():
+    """
+     Test that dev test only will not run for anything other than dev or test
+    """
+    original_settings = os.environ['DJANGO_SETTINGS_MODULE']
+
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'rapidresearchregistry.settings.wrong'
+    result = dev_test_only(fake_fn)
+    assert result == "This task only runs on the development and test environments. Believe me you don\'t want to run this in production."
+
+    #reset setting module to original
+    os.environ['DJANGO_SETTINGS_MODULE'] = original_settings
