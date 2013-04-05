7786765f2679975f0b150390e5f5a7689f93b751
Moses E. Miles III <vamem9z@gmail.com>
2013-04-05 13:41:33 +0000

    Refactor CreateUpdateModel to django_helpers

--- django_helpers/models.py
+++ django_helpers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class CreateUpdateModel(models.Model):
+    """
+     Abstract model class that adds created_at and updated_at fields
+    """
+    created_at = models.DateTimeField(auto_now_add=True,default=now)
+    updated_at = models.DateTimeField(auto_now=True,default=now)
+
+    class Meta:
+        abstract = True
