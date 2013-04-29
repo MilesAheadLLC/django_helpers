95d6e63692cc67a5d60eac14d4c0dfe75d3025f1
Moses E. Miles III <vamem9z@gmail.com>
2013-04-29 10:07:46 +0000

    Add testing helper

--- django_helpers/helpers/test.py
+++ django_helpers/helpers/test.py
@@ -1,4 +1,6 @@
 from django.core.urlresolvers import reverse
+from django.contrib.sites.models import Site
+from urlparse import urlparse
 
 def get_response(view_name, client):
     '''
@@ -12,3 +14,12 @@ def get_status_code(view_name, client):
      Convenience function to return just the status code of the response object from get_response
     '''
     return get_response(view_name, client).status_code
+
+
+def reset_domain(live_server_url):
+    """
+     Resets site domain to that of the live server
+    """
+    current_site = Site.objects.get_current()
+    url = urlparse(live_server_url)
+    current_site.domain = url.netloc
