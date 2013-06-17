235d3550365c3e0510a7e8c0cbc6cc855e8c0336
Moses E. Miles III <vamem9z@gmail.com>
2013-06-17 16:26:32 +0000

    Add comments for settings helper

--- django_helpers/helpers/settings.py
+++ django_helpers/helpers/settings.py
@@ -1,4 +1,9 @@
 def set_default(value, default_value):
+    """
+     Sets default value for a settings configuration variable
+
+     e.g. DEBUG = set_default(os.environ.get('DEBUG'), True))
+    """
     if (not value):
         return default_value
     return value
