b51289ca5ebb6aabaaaca4b4e9f69effc2b0162f
Moses E. Miles III <vamem9z@gmail.com>
2013-06-30 17:01:34 +0000

    Add WebTest test helper and setup dependencies

--- django_helpers/helpers/test.py
+++ django_helpers/helpers/test.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.contrib.sites.models import Site
 from urlparse import urlparse
+from django_webtest import WebTest
 
 def get_response(view_name, client):
     '''
@@ -15,7 +16,6 @@ def get_status_code(view_name, client):
     '''
     return get_response(view_name, client).status_code
 
-
 def reset_domain(live_server_url):
     """
      Resets site domain to that of the live server
@@ -23,3 +23,21 @@ def reset_domain(live_server_url):
     current_site = Site.objects.get_current()
     url = urlparse(live_server_url)
     current_site.domain = url.netloc
+
+class WebTestHelpers(WebTest):
+    """
+     Provides helpers for django webtest based tests
+    """
+    def get_status_code(self, view_name):
+        """
+         Returns status code for a django view using WebTest instead of Django's client
+        """
+        res = self.app.get(reverse(view_name))
+        return res.status_code
+
+    def get_response(self, view_name):
+        """
+         Returns the response to a view and follows the views redirect
+        """
+        res = self.app.get(reverse(view_name))
+        return res
--- setup.py
+++ setup.py
@@ -2,10 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django_helpers",
-    version = "0.1",
-    description = "Helper for Django Apps",
+    version = "0.2",
+    description = "Helpers for Django Apps",
     packages = find_packages('.'),
     package_dir = {"":"."},
     package_data ={},
-    install_requires = []
+    install_requires = [
+        'Django >= 1.5.1',
+        'pytest >= 2.3.4',
+        'pytest-django >= 2.3.0',
+        'fabric >= 1.6.1',
+        'pycrypto >= 2.6',
+        'selenium >= 2.32.0',
+        'webtest >= 2.0.6',
+        'django-webtest >= 1.7.2',
+    ]
 )
